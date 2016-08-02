56713196745173edac337d08326c906f40602c95
Julien Lamarche <julien.lamarche@gmail.com>
2016-08-01 23:34:00 -0400

    initial project

---
+++ main.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby -w
+
+puts "Lookingb in:"
+puts $:
+puts
+
+require 'rubygems'
+#require '/var/lib/gems/1.9.1/gems/twitter-5.16.0/lib/twitter.rb'
+require 'twitter'
+#require 'byebug'
+include __dir__ + '/clientConf.rb'
+
+
+client = Twitter::REST::Client.new($clientConf)
+
+puts "Client ready" if client
+
+sr = client.search("http://www.conservative.ca/cpc/protect-your-vote/")
+
+puts sr.count
+
+f = sr.first
+puts f
+puts f.methods.sort.join(', ')
+puts f.id
+puts f.url
