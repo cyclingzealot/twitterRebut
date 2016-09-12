efa843c6adb25ba9d26b41f9fd7b3cc3c98de391
Julien Lamarche <julien.lamarche@gmail.com>
2016-09-12 13:24:49 -0400

    added search term for URL of ronaambrose

--- main.rb
+++ main.rb
@@ -73,7 +73,7 @@ end
 
 
 puts "Getting search results..."
-searchTerms = ["conservative.ca/cpc/protect-your-vote", "demandareferendum.ca", "#DemandAReferendum", "ronaambrose/videos/10153984927048525/"]
+searchTerms = ["conservative.ca/cpc/protect-your-vote", "demandareferendum.ca", "#DemandAReferendum", "ronaambrose/videos/10153984927048525/", "Liberals' electoral reform debate denies Canadians their say"]
 
 sr = Set.new
 searchTerms.each { |st|
